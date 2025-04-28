0463dcdc7149b2ded140cf198a77bf422308eaec
Oleg Neychev <oleg.neychev@namecheap.com>
2025-04-28 11:20:59 +0300

    Package updates: mockery, go packages, etc.
    1. Upgrade Mockery and regenerate mocks
    2. Upgrade Go to 1.24
    3. Upgrade all go packages to their latest version
    
    Signed-off-by: Oleg Neychev <oleg.neychev@namecheap.com>

--- helper.sh
+++ helper.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-GOLANGCI_LINT_VERSION="v1.60.2"
-GOFUMPT_VERSION="v0.7.0"
-MOCKERY_VERSION="v2.50.0"
+GOLANGCI_LINT_VERSION="v2.1.5"
+GOFUMPT_VERSION="v0.8.0"
+MOCKERY_VERSION="v3.2.4"
 
 prerequisites() {
   if [[ "$(golangci-lint --version 2>&1)" != *"$GOLANGCI_LINT_VERSION"* ]]; then
-    go install github.com/golangci/golangci-lint/cmd/golangci-lint@"${GOLANGCI_LINT_VERSION}"
+    go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@"${GOLANGCI_LINT_VERSION}"
   fi
   if [[ "$(gofumpt --version 2>&1)" != *"$GOFUMPT_VERSION"* ]]; then
     go install mvdan.cc/gofumpt@"${GOFUMPT_VERSION}"
   fi
   if [[ "$(mockery --version 2>&1)" != *"$MOCKERY_VERSION"* ]]; then
-    go install github.com/vektra/mockery/v2@"${MOCKERY_VERSION}"
+    go install github.com/vektra/mockery/v3@"${MOCKERY_VERSION}"
   fi
 }
 
